c05e010e5b68237bc745541eba4989d0395dd64e
luzh <332862394@qq.com>
2022-01-21 10:34:25 +0800

    deploy

---
+++ precache-manifest.8f75cad67938aa33d693cac017ea4779.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8a647c32b92cfc882d",
+    "url": "/moaccountvue-website/css/app.426e2ced.css"
+  },
+  {
+    "revision": "2833687446a7a681039976111caa3c2b",
+    "url": "/moaccountvue-website/index.html"
+  },
+  {
+    "revision": "1d8a647c32b92cfc882d",
+    "url": "/moaccountvue-website/js/app.011910e3.js"
+  },
+  {
+    "revision": "3bab4f2d02fad3466ac1",
+    "url": "/moaccountvue-website/js/chunk-vendors.bd4bf2f6.js"
+  },
+  {
+    "revision": "076f316f87045ba1fd3dbe1710ad561f",
+    "url": "/moaccountvue-website/manifest.json"
+  },
+  {
+    "revision": "3b15ab411c08fe1725125cb0a8bef7cb",
+    "url": "/moaccountvue-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moaccountvue-website/robots.txt"
+  }
+]);
